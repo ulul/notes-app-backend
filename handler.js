bf71c08fbaadef3acae6776adf52fa9ce23e88b6
ulul <hudanuzulul@gmail.com>
2022-10-31 06:57:19 +0700

    Belajar framework HAPI pada javascript

---
+++ handler.js
@@ -0,0 +1,104 @@
+const { nanoid } = require('nanoid');
+const notes = require('./notes');
+
+const getAllNotesHandler = () => ({
+  status: 'success',
+  data: {
+    notes,
+  },
+});
+
+const addNoteHandler = (request, h) => {
+  const { title, tags, body } = request.payload;
+
+  const id = nanoid(16);
+  const createdAt = new Date().toString();
+  const updatedAt = createdAt;
+
+  const newNote = {
+    id, title, tags, body, createdAt, updatedAt,
+  };
+
+  notes.push(newNote);
+
+  const isSuccess = notes.filter((note) => note.id === id).length > 0;
+
+  if (isSuccess) {
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil ditambahkan',
+      data: {
+        noteId: id,
+      },
+    });
+    response.code = 201;
+    return response;
+  }
+  const response = h.response({
+    status: 'failed',
+    message: 'Catatan gagal ditambahkan',
+  });
+  response.code = 500;
+  return response;
+};
+
+const editNoteByIdHandler = (request, h) => {
+  const { id } = request.params;
+
+  const { title, tags, body } = request.payload;
+  const updatedAt = new Date().toString();
+
+  const index = notes.findIndex((note) => note.id === id);
+
+  if (index !== -1) {
+    notes[index] = {
+      ...notes[index],
+      title,
+      tags,
+      body,
+      updatedAt,
+    };
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil diperbaharui',
+    });
+    response.code = 200;
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan gagal diperbaharui',
+  });
+  response.code = 400;
+  return response;
+};
+
+const deleteNoteByIdHandler = (request, h) => {
+  const { id } = request.params;
+
+  const index = notes.findIndex((note) => note.id === id);
+
+  if (index !== -1) {
+    notes.splice(index, 1);
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil dihapus',
+    });
+    response.code = 200;
+    return response;
+  }
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan gagal dihapus',
+  });
+  response.code = 400;
+  return response;
+};
+
+module.exports = {
+  getAllNotesHandler,
+  addNoteHandler,
+  editNoteByIdHandler,
+  deleteNoteByIdHandler,
+};
